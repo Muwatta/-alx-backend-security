0e1ac1c3852243b5702248011c814ff0738b1307
Muwatta <abdullahmusliudeen@gamil.com>
2025-11-04 06:10:32 +0100

    Task 0: Basic IP logging middleware and RequestLog model

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,21 +1,20 @@
-from django.http import HttpResponseForbidden
-from .models import RequestLog, BlockedIP
+from .models import RequestLog
+from django.utils.timezone import now
+from django.utils.deprecation import MiddlewareMixin
 
-class IPLoggingMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+class IPLoggingMiddleware(MiddlewareMixin):
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')
 
-    def __call__(self, request):
-        ip = request.META.get('REMOTE_ADDR')
+    def process_request(self, request):
+        ip = self.get_client_ip(request)
+        path = request.path
 
-        # Check if blocked
-        if BlockedIP.objects.filter(ip_address=ip).exists():
-            return HttpResponseForbidden("Access Denied")
-
-        # Log request
         RequestLog.objects.create(
             ip_address=ip,
-            path=request.path,
+            path=path,
+            timestamp=now()
         )
-
-        return self.get_response(request)
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -5,8 +5,5 @@ class RequestLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
 
-class BlockedIP(models.Model):
-    ip_address = models.GenericIPAddressField(unique=True)
-
     def __str__(self):
-        return self.ip_address
+        return f"{self.ip_address} - {self.path} - {self.timestamp}"
