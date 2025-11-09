356bc6198d7febca05bbf789b9aa79785f3f25ea
Muwatta <abdullahmusliudeen@gamil.com>
2025-11-09 22:16:39 +0100

    updated

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'ip_tracking.middleware.IPTrackingMiddleware',
+    'ip_tracking.middleware.IPTrackingMiddleware',
 ]
 
 # URLs and WSGI
